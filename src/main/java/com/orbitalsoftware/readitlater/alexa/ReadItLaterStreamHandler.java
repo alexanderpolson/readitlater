96fc1d582b831c8cf65270e26cc21b9243646702
Alexander Polson <alex@alexpolson.com>
2018-11-15 09:38:58 -0800

    Move Skill instantiation to factory class so it can be used by other functionality.

--- src/main/java/com/orbitalsoftware/readitlater/alexa/ReadItLaterStreamHandler.java
+++ src/main/java/com/orbitalsoftware/readitlater/alexa/ReadItLaterStreamHandler.java
@@ -1,40 +1,10 @@
 package com.orbitalsoftware.readitlater.alexa;
 
-import com.amazon.ask.Skill;
 import com.amazon.ask.SkillStreamHandler;
-import com.amazon.ask.Skills;
-import com.orbitalsoftware.readitlater.alexa.intent.ArchiveArticleIntentHandler;
-import com.orbitalsoftware.readitlater.alexa.intent.CancelAndStopIntentHandler;
-import com.orbitalsoftware.readitlater.alexa.intent.DeleteArticleIntentHandler;
-import com.orbitalsoftware.readitlater.alexa.intent.HelpIntentHandler;
-import com.orbitalsoftware.readitlater.alexa.intent.LaunchIntentHandler;
-import com.orbitalsoftware.readitlater.alexa.intent.ReadArticleIntentHandler;
-import com.orbitalsoftware.readitlater.alexa.intent.SkipArticleIntentHandler;
-import com.orbitalsoftware.readitlater.alexa.intent.StarArticleIntentHandler;
 
 public class ReadItLaterStreamHandler extends SkillStreamHandler {
 
-  private static final String STATE_TABLE_NAME = "ReadItLaterState";
-
-  private static Skill getSkill() {
-    return Skills.standard()
-        .withTableName(STATE_TABLE_NAME)
-        .withAutoCreateTable(true)
-        .addExceptionHandler(new ReadItLaterExceptionHandler())
-        .addRequestHandlers(
-            new LaunchIntentHandler(),
-            new ArchiveArticleIntentHandler(),
-            new DeleteArticleIntentHandler(),
-            new SkipArticleIntentHandler(),
-            new StarArticleIntentHandler(),
-            new ReadArticleIntentHandler(),
-            new CancelAndStopIntentHandler(),
-            new HelpIntentHandler(),
-            new SessionEndedRequestHandler())
-        .build();
-  }
-
   public ReadItLaterStreamHandler() {
-    super(getSkill());
+    super(ReadItLaterSkillFactory.createInstance());
   }
 }
